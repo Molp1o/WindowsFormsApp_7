ac09b72a5dddec945ec4ede3d4ede91518d0c923
Molp1o <117813570+Molp1o@users.noreply.github.com>
2023-06-27 19:35:59 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,19 +37,24 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.gradus_lbl = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.surfacing_pb = new System.Windows.Forms.PictureBox();
+            this.about_pb = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nerd_detector)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.surfacing_pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.about_pb)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(579, 526);
+            this.pictureBox1.Location = new System.Drawing.Point(590, 524);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(255, 197);
+            this.pictureBox1.Size = new System.Drawing.Size(91, 81);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -59,9 +64,9 @@
             // 
             this.nerd_detector.BackColor = System.Drawing.Color.Transparent;
             this.nerd_detector.Image = ((System.Drawing.Image)(resources.GetObject("nerd_detector.Image")));
-            this.nerd_detector.Location = new System.Drawing.Point(12, 12);
+            this.nerd_detector.Location = new System.Drawing.Point(695, 12);
             this.nerd_detector.Name = "nerd_detector";
-            this.nerd_detector.Size = new System.Drawing.Size(325, 325);
+            this.nerd_detector.Size = new System.Drawing.Size(210, 210);
             this.nerd_detector.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.nerd_detector.TabIndex = 1;
             this.nerd_detector.TabStop = false;
@@ -70,9 +75,10 @@
             // military_link
             // 
             this.military_link.AutoSize = true;
-            this.military_link.BackColor = System.Drawing.Color.Black;
+            this.military_link.BackColor = System.Drawing.Color.Transparent;
             this.military_link.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.military_link.Location = new System.Drawing.Point(656, 862);
+            this.military_link.ForeColor = System.Drawing.Color.Aqua;
+            this.military_link.Location = new System.Drawing.Point(602, 855);
             this.military_link.Name = "military_link";
             this.military_link.Size = new System.Drawing.Size(101, 24);
             this.military_link.TabIndex = 2;
@@ -82,9 +88,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(494, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(418, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(437, 436);
+            this.pictureBox2.Size = new System.Drawing.Size(210, 210);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -92,10 +98,11 @@
             // 
             // pictureBox3
             // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(1176, 739);
+            this.pictureBox3.Location = new System.Drawing.Point(985, 267);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.Size = new System.Drawing.Size(89, 93);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
@@ -104,24 +111,53 @@
             // gradus_lbl
             // 
             this.gradus_lbl.AutoSize = true;
-            this.gradus_lbl.ForeColor = System.Drawing.Color.Black;
-            this.gradus_lbl.Location = new System.Drawing.Point(1229, 710);
+            this.gradus_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.gradus_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gradus_lbl.ForeColor = System.Drawing.Color.Aqua;
+            this.gradus_lbl.Location = new System.Drawing.Point(1080, 287);
             this.gradus_lbl.Name = "gradus_lbl";
-            this.gradus_lbl.Size = new System.Drawing.Size(41, 13);
+            this.gradus_lbl.Size = new System.Drawing.Size(36, 25);
             this.gradus_lbl.TabIndex = 5;
-            this.gradus_lbl.Text = "gradusi";
+            this.gradus_lbl.Text = "20";
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // surfacing_pb
+            // 
+            this.surfacing_pb.BackColor = System.Drawing.Color.Transparent;
+            this.surfacing_pb.Image = ((System.Drawing.Image)(resources.GetObject("surfacing_pb.Image")));
+            this.surfacing_pb.Location = new System.Drawing.Point(86, 663);
+            this.surfacing_pb.Name = "surfacing_pb";
+            this.surfacing_pb.Size = new System.Drawing.Size(96, 96);
+            this.surfacing_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.surfacing_pb.TabIndex = 7;
+            this.surfacing_pb.TabStop = false;
+            this.surfacing_pb.Click += new System.EventHandler(this.surfacing_pb_Click);
+            // 
+            // about_pb
+            // 
+            this.about_pb.BackColor = System.Drawing.Color.Transparent;
+            this.about_pb.Image = ((System.Drawing.Image)(resources.GetObject("about_pb.Image")));
+            this.about_pb.Location = new System.Drawing.Point(1226, 789);
+            this.about_pb.Name = "about_pb";
+            this.about_pb.Size = new System.Drawing.Size(100, 100);
+            this.about_pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.about_pb.TabIndex = 8;
+            this.about_pb.TabStop = false;
+            this.about_pb.Click += new System.EventHandler(this.about_pb_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1338, 901);
+            this.Controls.Add(this.about_pb);
+            this.Controls.Add(this.surfacing_pb);
             this.Controls.Add(this.gradus_lbl);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -130,15 +166,14 @@
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.Color.Transparent;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Text = "Подводная лодка";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Shown += new System.EventHandler(this.Form1_Shown);
-            this.Enter += new System.EventHandler(this.Form1_Enter);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nerd_detector)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.surfacing_pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.about_pb)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,6 +188,9 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label gradus_lbl;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox surfacing_pb;
+        private System.Windows.Forms.PictureBox about_pb;
     }
 }
 
---
+++ Strategy.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp_7
+{
+    public interface Sub_Movs_Strategy
+    {
+        
+        byte Ventilation(bool ventilation_enabled, byte gradus);
+
+    }
+    public interface Sub_Mil_Acts_Strategy
+    {
+        bool Attack(string rocket_type);
+        bool Self_Destruction();
+        bool Nerd_Destruction();
+    }
+    public interface Radar_Strategy
+    {
+        bool Detection(bool enemy_flag);
+    }
+
+    
+    public class Submarine_movements : Sub_Movs_Strategy
+    {
+        
+        public byte Ventilation(bool ventilation_enabled, byte gradus)
+        {
+            if (ventilation_enabled == true)
+            {
+                gradus++;
+            }
+            else
+            {
+                gradus--;
+            }
+            return gradus;
+
+        }
+    }
+    public class Submarine_military_actions : Sub_Mil_Acts_Strategy
+    {
+        public bool Attack(string rocket_type)
+        {
+            if (string.IsNullOrEmpty(rocket_type))
+            {
+                return false;
+            }
+            else
+            {
+                MessageBox.Show("Враг был уничтожен ракетой " + rocket_type + "!", "Враг уничтожен!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return true;
+            }
+        }
+
+        public bool Self_Destruction()
+        {
+            bool pentaflag;
+            Pentagon pentagon = new Pentagon();
+            pentagon.Show();
+            if (pentagon.Visible)
+            {
+                pentaflag = true;
+            }
+            else
+            {
+                pentaflag = false;
+            }
+            return pentaflag;
+        }
+        public bool Nerd_Destruction()
+        {
+            MessageBox.Show("Зануда был уничтожен", "Уничтожение зануды", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return true;
+        }
+
+    }
+
+    public class Radar : Radar_Strategy
+    {
+        
+        public bool Detection(bool enemy_flag)
+        {
+            if (!enemy_flag)
+            {
+                return false;
+            }
+            else
+            {
+                MessageBox.Show("Вы не можете продолжить сканирование, пока враг не уничтожен!!", "ВНИМАНИЕ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return true;
+            }
+        }
+    }
+    public class Submarine_radar
+    {
+        public bool enemy_flag { get; set; }
+        Radar_Strategy _radar_strategy;
+        public Submarine_radar(Radar_Strategy radar_strategy)
+        {
+            _radar_strategy = radar_strategy;
+        }
+        public void Scan()
+        {
+            _radar_strategy.Detection(enemy_flag);
+        }
+    }
+    public class Military_Acts
+    {
+        public string rocket_type { get; set; }
+        Sub_Mil_Acts_Strategy _sub_mil_acts;
+        public Military_Acts(Sub_Mil_Acts_Strategy sub_mil_acts)
+        {
+            _sub_mil_acts = sub_mil_acts;
+        }
+        public void RED_BUTTON()
+        {
+
+            _sub_mil_acts.Self_Destruction();
+        }
+        public void Nerd_radar()
+        {
+            _sub_mil_acts.Nerd_Destruction();
+        }
+        public void Rocket_Attack()
+        {
+            _sub_mil_acts.Attack(rocket_type);
+        }
+    }
+    public class Movements
+    {
+        Sub_Movs_Strategy _sub_movs;
+        public Movements(Sub_Movs_Strategy sub_movs)
+        {
+            _sub_movs = sub_movs;
+        }
+        public byte gradus = 20;
+        public void Ventilation(bool ventilation_enabled)
+        {
+            gradus = _sub_movs.Ventilation(ventilation_enabled, gradus);
+        }
+    }
+}
